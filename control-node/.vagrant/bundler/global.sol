b6494b3b44301e959343cee14f087933a027582e
Átila Delcanton Rampazo <atila.rampazo@atila.rampazo>
2023-11-27 16:11:55 -0300

    Vagrant control-pane

---
+++ control-node/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"8486bf49d220623bbb8f5aaa6cf976019c51634556db3b990e4179fc9cbd5b58","vagrant_version":"2.4.0"}
